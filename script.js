f2df2aa7ddeb815fd686f92207ed6ba0a1b5fc0c
OLá Mundo <anabelanams@gmail.com>
2024-10-02 09:08:51 -0400

    Update

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("JavaScript carregado com sucesso!");
 
+    setTimeout(function() {
+        document.title = "Título Alterado!";
+        console.log("Título da página foi alterado.");
+    }, 2000);
 
+    
 
     console.log("JavaScript carregado com sucesso!");
