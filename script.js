1fdf5f5c4cef6dffd146fe4f3e86401c5415bb21
OLá Mundo <anabelanams@gmail.com>
2024-10-02 09:09:56 -0400

    Remoção - Delete

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("JavaScript carregado com sucesso!");
 
-    setTimeout(function() {
-        document.title = "Título Alterado!";
-        console.log("Título da página foi alterado.");
-    }, 2000);
-
     const imagens = document.querySelectorAll("img");
     imagens.forEach((imagem) => {
         imagem.addEventListener("click", function() {
