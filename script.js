5f621583d1a7bf58688e481669b6e4c98853ee2c
OLá Mundo <anabelanams@gmail.com>
2024-10-02 09:09:15 -0400

    Update - Inserção

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ document.addEventListener("DOMContentLoaded", function() {
         console.log("Título da página foi alterado.");
     }, 2000);
 
-    
+    const imagens = document.querySelectorAll("img");
+    imagens.forEach((imagem) => {
+        imagem.addEventListener("click", function() {
+            alert("Você clicou na imagem: " + this.alt);
+        });
+    });
+});
 
     console.log("JavaScript carregado com sucesso!");
